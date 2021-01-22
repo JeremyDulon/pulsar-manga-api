175e989620f306b9be0dc686309216cd0c580413
Jeremy Dulon <jeremy.dulon@live.fr>
2021-01-22 16:34:44 +0100

    update token length

--- src/Entity/AccessToken.php
+++ src/Entity/AccessToken.php
@@ -8,6 +8,15 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Table(name="oauth2_access_tokens")
  * @ORM\Entity
+ * @ORM\AttributeOverrides({
+ *     @ORM\AttributeOverride(name="token",
+ *         column=@ORM\Column(
+ *             name   = "token",
+ *             type   = "string",
+ *             length = 128
+ *         )
+ *     )
+ * })
  */
 class AccessToken extends BaseAccessToken
 {
--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -8,6 +8,15 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Table(name="oauth2_refresh_tokens")
  * @ORM\Entity
+ * @ORM\AttributeOverrides({
+ *     @ORM\AttributeOverride(name="token",
+ *         column=@ORM\Column(
+ *             name   = "token",
+ *             type   = "string",
+ *             length = 128
+ *         )
+ *     )
+ * })
  */
 class RefreshToken extends BaseRefreshToken
 {
